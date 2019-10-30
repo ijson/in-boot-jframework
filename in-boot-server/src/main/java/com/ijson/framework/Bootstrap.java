47955c18dd4461f38ec48b85bbd474d747cea594
cuiyongxu <cuiyongxu@gmail.com>
2019-10-30 22:54:18 +0800

    add ehcache

--- in-boot-server/src/main/java/com/ijson/framework/Bootstrap.java
+++ in-boot-server/src/main/java/com/ijson/framework/Bootstrap.java
@@ -2,8 +2,12 @@ package com.ijson.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ImportResource;
 
+@EnableCaching
 @SpringBootApplication
+@ImportResource(locations = {"classpath:applicationContext.xml"})
 public class Bootstrap {
 
 	public static void main(String[] args) {
